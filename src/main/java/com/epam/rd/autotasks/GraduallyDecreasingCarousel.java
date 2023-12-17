d6b33ba333b536812e091cf55b46a03e64206a50
Avdeenko Ekaterina <avdeenqoeka@gmail.com>
2023-12-17 20:45:35 +0100

    gradually-decreasing-carousel_AvdeenkoEkaterina

--- src/main/java/com/epam/rd/autotasks/GraduallyDecreasingCarousel.java
+++ src/main/java/com/epam/rd/autotasks/GraduallyDecreasingCarousel.java
@@ -1,16 +1,17 @@
 package com.epam.rd.autotasks;
 
-public class GraduallyDecreasingCarousel extends DecrementingCarousel{
-    public GraduallyDecreasingCarousel(final int capacity) {
-        super(capacity);
-    }
+public class GraduallyDecreasingCarousel extends DecrementingCarousel {
 
-    @Override
-    public CarouselRun run() {
-        if (!isRun) {
-            isRun = true;
-            return new GraduallyDecreasingCarouselRun();
-        }
-        return null;
+  public GraduallyDecreasingCarousel(final int capacity) {
+    super(capacity);
+  }
+
+  @Override
+  public CarouselRun run() {
+    if (!isRun) {
+      isRun = true;
+      return new GraduallyDecreasingCarouselRun();
     }
+    return null;
+  }
 }
